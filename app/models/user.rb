78ed9729a144657a14edb56d5e876306728afb3b
James Darling <james@abscond.org>
2019-03-08 19:35:41 +0000

    Stub out IP logging fields
    I want to use Devise's `trackable` module to be able to track when users loast logged in, as I think that will be helpful in the future.
    
    However, I don't need or want to be logging user's IP addresses. I feels a bit unnecessary and invasive. However Devise really wants to do this if trackable is on. So to bypass this, I'm adding these attr_accessors which allow devise to assign them, but they won't be persisted in the DB.

--- app/models/user.rb
+++ app/models/user.rb
@@ -3,4 +3,6 @@ class User < ApplicationRecord
   # :lockable, :timeoutable and :omniauthable
   devise :database_authenticatable, :registerable, :trackable,
          :recoverable, :rememberable, :validatable, :confirmable
+
+  attr_accessor :last_sign_in_ip, :current_sign_in_ip
 end
