2888fc5ae61663011b58d7f30e123ee499ea716e
James Darling <james@abscond.org>
2019-09-25 15:55:01 +0100

    Allow admins to edit users
    Specifically around managing admins and early access.

--- app/controllers/admin/users_controller.rb
+++ app/controllers/admin/users_controller.rb
@@ -2,4 +2,20 @@ class Admin::UsersController < AdminController
   def index
     @users = User.confirmed.order(:created_at).page params[:page]
   end
+
+  def edit
+    @user = User.find(params[:id])
+  end
+
+  def update
+    @user = User.find(params[:id])
+    @user.update!(user_params)
+    redirect_to action: :index
+  end
+
+  private
+
+  def user_params
+    params.require(:user).permit(:name, :email, :admin, :early_access)
+  end
 end
