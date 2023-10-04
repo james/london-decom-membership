1abf7967b4923527250028d7774a89b70b6736ba
James Darling <james@abscond.org>
2023-10-04 22:38:05 +0100

    Reactivate tickets and low income applications

--- spec/features/tickets_spec.rb
+++ spec/features/tickets_spec.rb
@@ -9,41 +9,29 @@ RSpec.feature 'Tickets', type: :feature do
   end
 
   scenario 'there is no active event, but the member has early access' do
-    pending 'deactivated for the year'
-    stub_eventbrite_event(available_tickets_for_code: 2, tickets_sold_for_code: 0)
+    stub_eventbrite_event(available_tickets_for_code: 1, tickets_sold_for_code: 0)
     create(:event, active: false)
     login(early_access: true)
 
-    expect(page).to have_text('Buy Tickets')
-    expect(page).to have_text('You can buy 2 tickets')
+    expect(page).to have_text('Buy Ticket')
+    expect(page).to have_text('You can buy 1 ticket')
   end
 
-  scenario 'user has 2 available tickets and bought none' do
-    pending 'deactivated for the year'
-    stub_eventbrite_event(available_tickets_for_code: 2, tickets_sold_for_code: 0)
-    create(:event)
-    login
-
-    expect(page).to have_text('Buy Tickets')
-    expect(page).to have_text('You can buy 2 tickets')
-  end
-
-  scenario 'user has 2 available tickets and bought 1' do
-    pending 'deactivated for the year'
-    stub_eventbrite_event(available_tickets_for_code: 1, tickets_sold_for_code: 1)
+  scenario 'user has 1 available tickets and bought none' do
+    stub_eventbrite_event(available_tickets_for_code: 1, tickets_sold_for_code: 0)
     create(:event)
     login
 
     expect(page).to have_text('Buy Ticket')
-    expect(page).to have_text('You have already bought 1 ticket. You can buy 1 more ticket')
+    expect(page).to have_text('You can buy 1 ticket')
   end
 
-  scenario 'user has 2 available tickets and bought 2' do
-    stub_eventbrite_event(available_tickets_for_code: 0, tickets_sold_for_code: 2)
+  scenario 'user has 1 available tickets and bought 1' do
+    stub_eventbrite_event(available_tickets_for_code: 0, tickets_sold_for_code: 1)
     create(:event)
     login
 
-    expect(page).to_not have_text('Buy Tickets')
-    expect(page).to have_text('You have bought the 2 tickets available to you')
+    expect(page).to_not have_text('Buy Ticket')
+    expect(page).to have_text('You have bought the 1 ticket available to you')
   end
 end
