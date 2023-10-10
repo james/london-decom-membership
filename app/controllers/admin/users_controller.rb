d70f31725b6918a8477f88bf515ccd3284317a14
riley <riley@hidinginthevoid.com>
2023-10-10 19:21:23 +0000

    [User] Make admin update call update_mailchimp

--- app/controllers/admin/users_controller.rb
+++ app/controllers/admin/users_controller.rb
@@ -19,6 +19,7 @@ class Admin::UsersController < AdminController
   def update
     @user = User.find(params[:id])
     @user.update!(user_params)
+    @user.update_mailchimp
     redirect_to action: :index
   end
 
