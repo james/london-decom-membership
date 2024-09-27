a290109969d69638b35f0d8dc50aa54fdbacd658
riley <riley@hidinginthevoid.com>
2024-09-26 17:58:25 -0700

    [Event] Testing Low Income Changes

--- spec/features/low_income_spec.rb
+++ spec/features/low_income_spec.rb
@@ -10,7 +10,6 @@ RSpec.feature 'Low Income', type: :feature do
   end
 
   scenario 'prerelease event and user has 1 available tickets can apply for low income' do
-    pending 'disabled for the year'
     stub_eventbrite_event(available_tickets_for_code: 1, tickets_sold_for_code: 0)
     create(:event, :prerelease)
     login
@@ -26,15 +25,6 @@ RSpec.feature 'Low Income', type: :feature do
     expect(page).to_not have_text('Apply for low income')
   end
 
-  scenario 'live event and user has 1 available tickets can apply for low income' do
-    pending 'disabled for the year'
-    stub_eventbrite_event(available_tickets_for_code: 1, tickets_sold_for_code: 0)
-    create(:event, :live)
-    login
-
-    expect(page).to have_text('Apply for low income')
-  end
-
   scenario 'live event and user has 0 available tickets can no longer apply for low income' do
     stub_eventbrite_event(available_tickets_for_code: 0, tickets_sold_for_code: 1)
     create(:event, :live)
@@ -43,10 +33,9 @@ RSpec.feature 'Low Income', type: :feature do
     expect(page).to_not have_text('Apply for low income')
   end
 
-  scenario 'user has 1 available tickets and bought none' do
-    pending 'disabled for the year'
+  scenario 'user has 1 available tickets and bought none, gets low income approved' do
     stub_eventbrite_event(available_tickets_for_code: 1, tickets_sold_for_code: 0)
-    create(:event)
+    create(:event, :prerelease)
     login
 
     click_link 'Apply for low income', match: :first
@@ -64,6 +53,20 @@ RSpec.feature 'Low Income', type: :feature do
     expect(page).to have_text('Your request for Low Income has been approved.')
     expect(page.html).to include(LowIncomeRequest.last.low_income_code.code)
     expect(page.html).to_not include(User.last.membership_code.code)
+  end
+
+  scenario 'user has 1 available tickets and bought none, gets low income rejected' do
+    stub_eventbrite_event(available_tickets_for_code: 1, tickets_sold_for_code: 0)
+    create(:event, :prerelease)
+    login
+
+    click_link 'Apply for low income', match: :first
+    fill_in 'Please let us know why you believe you need a low income ticket', with: 'My reason'
+    click_button 'Submit request'
+
+    expect(page).to_not have_text('Apply for low income')
+    expect(page).to have_text('You have applied for a Low Income Ticket.')
+    expect(page.html).to include(User.last.membership_code.code)
 
     LowIncomeRequest.last.reject!
 
@@ -71,5 +74,21 @@ RSpec.feature 'Low Income', type: :feature do
 
     expect(page).to have_text('Your request for Low Income has been rejected.')
     expect(page.html).to include(User.last.membership_code.code)
+  end
+
+  scenario 'prerelease event and user has 1 available tickets but the low income window has closed' do
+    stub_eventbrite_event(available_tickets_for_code: 1, tickets_sold_for_code: 0)
+    create(:event, :prerelease, low_income_requests_end: Time.zone.now.advance(weeks: -1))
+    login
+
+    expect(page).to_not have_text('Apply for low income')
+  end
+
+  scenario 'live event and user has 1 available tickets but the low income window has closed' do
+    stub_eventbrite_event(available_tickets_for_code: 1, tickets_sold_for_code: 0)
+    create(:event, low_income_requests_end: Time.zone.now.advance(weeks: -1))
+    login
+
+    expect(page).to_not have_text('Apply for low income')
   end
 end
